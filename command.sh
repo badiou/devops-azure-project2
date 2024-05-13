e50a0e6f6904fe96b86094eed5d0f65a56eddada
badiou <ourobadiou@gmail.com>
2024-05-13 22:29:41 +0200

    command.sh file

---
+++ command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# clone project
+git clone https://github.com/badiou/devops-azure-project2.git
+cd devops-azure-project2
+
+# Update repo
+git pull
+
+# install all requirements
+make all
+
+# Deploy web app: My app name is flask-ml-badiou. You can change it with your app name
+az webapp up --name flask-ml-badiou --location eastus --sku B1
